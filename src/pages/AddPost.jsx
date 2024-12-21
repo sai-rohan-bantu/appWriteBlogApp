ec5bad4e50940f03ae8a667c77c8113be44c60fc
sai-rohan-bantu <rohansiri2001@gmail.com>
2024-12-21 16:29:24 +0530

    commited changes

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Container, PostForm } from '../components'
+import { Container } from '../components'
+import PostForm from '../components/Post-Form/Postform'
 
 function AddPost() {
   return (
--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react'
-import {Container, PostForm} from '../components'
+import {Container} from '../components'
 import appwriteService from "../appwrite/config";
 import { useNavigate,  useParams } from 'react-router-dom';
-
+import PostForm from '../components/Post-Form/Postform'
 function EditPost() {
     const [post, setPosts] = useState(null)
     const {slug} = useParams()
